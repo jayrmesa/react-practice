f18c5094a77dfb716a7eff9de154f2717005a6a4
Jayr Mesa <jayrmesa@gmail.com>
2023-11-15 21:57:08 -0700

    added simple counter button

---
+++ counter/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+   const [counter, setCounter] = useState(0);
+
+   return(
+    <div className = "App">
+      <h1> Hello In Counting </h1>
+      <h2> Show me the numbahs </h2>
+      <p>
+        {counter}
+      </p>
+      <button onClick={() => {
+        setCounter(counter + 1);
+      }}>Increase Counter
+      </button>
+    </div>
+   )
+}
+
+export default App;
