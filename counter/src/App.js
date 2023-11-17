2c65531c468aef7d4b89507b7c65b70c7b8a829a
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-11-16 18:06:59 -0700

    added fetch button and logic for the data

--- counter/src/App.js
+++ counter/src/App.js
@@ -7,6 +7,20 @@ import './App.css';
 function App() {
    const [counter, setCounter] = useState(0);
 
+const fetchRandomData = () => {
+  return axios.get('https://randomuser.me/api')
+  .then(res =>  {
+    // handle success
+    console.log(res);
+    return res;
+  })
+  .catch(err => {
+    // handle error
+    console.error(err);
+  })
+}
+
+
    return(
     <div className = "App">
       <h1> Hello In Counting </h1>
@@ -17,6 +31,10 @@ function App() {
       <button onClick={() => {
         setCounter(counter + 1);
       }}>Increase Counter
+      </button>
+      <button onClick={() => {
+        fetchRandomData();
+      }}>fetch Data
       </button>
     </div>
    )
